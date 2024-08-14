6d1eb84fc07b729a2767d60938a33adf6e172071
Antonio880 <antoniocruzgomes880@gmail.com>
2024-08-14 16:57:27 -0300

    feat: ajuste do CRUD de usuário baseado nos requisitos

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,14 +4,16 @@ import {
   createUser,
   updateUser,
   deleteUser,
+  loginUser,
 } from "../controllers/UserController";
 import { validateToken } from "../middlewares/AuthMiddleware";
 
 const router = Router();
 
 router.get("/:id", validateToken, getUserById);
-router.post("/", validateToken, createUser);
-router.put("/:id", validateToken, updateUser);
-router.delete("/:id", validateToken, deleteUser);
+router.post("/register", createUser);
+router.post("/login", loginUser);
+router.put("/:id", validateToken, updateUser); 
+router.delete("/:id", validateToken, deleteUser); 
 
 export default router;
